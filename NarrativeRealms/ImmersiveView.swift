20461c66aa369c3e22a3b930d3963f628ec33acf
Liana O'Cleirigh <67060996+lianaixd@users.noreply.github.com>
2024-11-01 21:33:27 +0000

    Start window and first tutorial window
    Start screen, "New Story" button opens an ImmersiveSpace called FantasyScene. It also changes the window to the first tutorial window.

--- NarrativeRealms/ImmersiveView.swift
+++ NarrativeRealms/ImmersiveView.swift
@@ -1,10 +1,3 @@
-//
-//  ImmersiveView.swift
-//  NarrativeRealms
-//bananas
-//  Created by Liana O'Cleirigh on 02/06/2024.
-//
-
 import SwiftUI
 import RealityKit
 import RealityKitContent
@@ -13,7 +6,7 @@ struct ImmersiveView: View {
     var body: some View {
         RealityView { content in
             // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+            if let scene = try? await Entity(named: "FantasyScene", in: realityKitContentBundle) {
                 content.add(scene)
             }
         }
